ccebdfb76338f4b30c48a302179dac9486206fc6
Heavenlyaura <goodnessokafor462@gmail.com>
2023-09-12 14:16:13 +0100

    Prep 4 assigment submission

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int > numList = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter a number and enter 0 to exit: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+            numList.Add(userInput);
+            }
+
+            else{
+                break;
+            }
+        }
+
+        // calculate the sum of the list
+        int sum = 0;
+
+        foreach (int num in numList)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"sum: {sum }");
+
+        // calculate the average 
+        int length = numList.Count;
+
+        float average = sum / length;
+        Console.WriteLine($"Average: {average}");
+
+        // calcuate the largest number in the list
+        int max = 0;
+        foreach (int n in numList)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        Console.WriteLine($"Max Number: {max}");
+
+        // find the smallest positive number in the list
+
+        int min = sum; // i made the min number to be the sum of the list, this always ensures that the min is alway greater than an item in the list
+        foreach (int i in numList)
+        {
+            if (i < min && i > 0)
+            {
+                min = i;
+            }
+        }
+        Console.WriteLine($"Min: {min}");
+
+    
+
+
+
+
+
+
+
+        
     }
+    
 }
