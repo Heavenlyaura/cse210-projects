fc54f62ada6d4ab8fbef06719b6cd3da922948ae
Heavenlyaura <goodnessokafor462@gmail.com>
2023-09-12 09:09:35 +0100

    prep3 assignment submission

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgen = new Random();
+        int magicNumber = randomgen.Next(1,100);
+        int guessCount = 0; // this variable will keep count of how many guess was made by the user
+
+        while (true) // the while loop will always run
+        {
+            Console.Write("Enter you guess: ");
+            int guess = int.Parse(Console.ReadLine());
+            guessCount ++; // increments the guess count by one in each iteration
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Guess Lower!");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Guess Higher!");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!!!");
+                Console.WriteLine($"you had {guessCount} guesses");
+
+                Console.Write("Do you want to play again? ");
+                string playAgain = Console.ReadLine().ToLower();
+                guessCount = 0; // resets the guess count to zero
+
+                if (playAgain == "yes")
+                {
+                    magicNumber = randomgen.Next(1,100); // generate a new random number between 1 - 100
+                    continue; 
+                }
+
+                else if (playAgain == "no")
+                {
+                    break;
+                }
+            }
+        }
+
+        
     }
 }
