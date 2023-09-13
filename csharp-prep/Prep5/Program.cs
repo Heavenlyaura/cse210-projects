a07d2357e12e7af14a0db4fc8826308eade8fdb6
Heavenlyaura <goodnessokafor462@gmail.com>
2023-09-13 18:27:55 +0100

    prep 5 assignment submission

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter you Name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter you favourite number: ");
+        int userInput = int.Parse(Console.ReadLine());
+        return userInput;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int square = SquareNumber(favNumber);
+        DisplayResult(userName, square);
     }
 }
