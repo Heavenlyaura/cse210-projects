52e76de85b5a392d34c110b9a8df3d234400b083
Heavenlyaura <goodnessokafor462@gmail.com>
2023-09-18 10:53:03 +0100

    submitted learning 02 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using Learning02;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+        job1._company = "Heavenly Aura";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new();
+        job2._company = "Heavenly Aura";
+        job2._jobTitle = "Technical Writer";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+        Resume myResume = new();
+
+        myResume._name = "goodness Okafor";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
+
+
+
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        // job1.DisplayJobDetails();
     }
+
+
+
+
+
+
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,27 @@
+﻿namespace Learning02;
+
+// public class Resume
+// {
+//     public string _fullName;
+//     public List<Job> _jobs = new();  
+// }
+public class Resume
+{
+    public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.DisplayJobDetails();
+        }
+    }
+}
