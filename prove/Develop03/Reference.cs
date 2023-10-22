1961befd0532581cc43aa38d6931744d96e93b68
HeavenlyAura <goodnessokafor462@gmail.com>
2023-10-22 07:10:17 +0100

    modification

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -3,8 +3,8 @@
 public class Reference
 {
     private string _reference = "Moroni 10:3-4";
-    private string _verse = " Behold, I would exhort you that when ye shall read these things, if it be wisdom in God that ye should read them, that ye would remember how merciful the Lord hath been unto the children of men, from the creation of Adam even down until the time that ye shall receive these things, and ponder it in your hearts.";
-    private string _endVerse = "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost.";
+    private string _verse = " 3. Behold, I would exhort you that when ye shall read these things, if it be wisdom in God that ye should read them, that ye would remember how merciful the Lord hath been unto the children of men, from the creation of Adam even down until the time that ye shall receive these things, and ponder it in your hearts.";
+    private string _endVerse = "4. And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost.";
 
     public string GetReference()
     {
