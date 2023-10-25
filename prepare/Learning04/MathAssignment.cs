cd810147d549c2754e25dd8132b82e7df9976eb3
HeavenlyAura <goodnessokafor462@gmail.com>
2023-10-25 13:16:43 +0100

    learing 04 assignment submission

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+﻿namespace Learning04;
+
+public class MathAssignment : Assignment
+{
+   private string _textbooksection;
+   private string _problems;
+
+   public MathAssignment(string studentName, string topic, string textBookSection, string problem) : base(studentName, topic)
+   {
+        _textbooksection = textBookSection;
+        _problems = problem;
+   }
+
+   public string GetHomeworkList()
+   {
+    return $"Section: {_textbooksection} Problem: {_problems}";
+   }
+}
