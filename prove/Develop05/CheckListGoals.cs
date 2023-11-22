bdb17977a2c80f2cb438bd1fe46bda7f5062a87c
HeavenlyAura <goodnessokafor462@gmail.com>
2023-11-22 08:12:10 -0700

    Submission

--- prove/Develop05/CheckListGoals.cs
+++ prove/Develop05/CheckListGoals.cs
@@ -1,40 +1,127 @@
-﻿namespace Develop05;
-
-public class CheckListGoals : Goals
+﻿using System;
+//Public class to can use in "other window"
+public class CheckListGoal : Goal // SimpleGoal child class of Goal
 {
-    private int _times;
-    private bool _iscomplete = false;
-    
-    public override void Play()
+    //Protect class inside the propieties to not affect the working of the program (only the inherit class)
+    //Protect class inside the propieties to not affect the working of the program (only the inherit class)
+    //protected string _title;
+    //protected string _description;
+    //protected int _pointsToComplete;
+    //protected List<string> _listOfGoals = new List<string>();
+
+    //This class propieties
+    private int _currentlyCounter;
+    private int _verificationCounter;
+    private int _bonusGoalCompleted;
+    private bool _checked = false;
+
+    //------------------ Constructors section (templates)--------------
+
+    //The default CheckList when we create (all the parameters from Goal)
+     public CheckListGoal(string goalName, string goalDescription, int pointsArchieved, int verificationCounter, int bonusGoalCompleted, int currentCounter = 0, bool Goalchecked = false) : base(goalName, goalDescription, pointsArchieved)
     {
-        Console.Write("What is the name of your goal? ");
-        string goalName = Console.ReadLine();
-        _name.Add(goalName);
+        //The normal propieties, the check-counter and the bonus when complete the goal
+        _title = goalName;
+        _description = goalDescription;
+        _pointsToComplete = pointsArchieved;
+        _verificationCounter = verificationCounter;
+        _bonusGoalCompleted = bonusGoalCompleted;
+        _currentlyCounter = currentCounter;
+        _checked = Goalchecked;
+    }
+    //----------- Constructors section (templates)--------------
+    //------------- (Methods - Behaviors) -----------------------
+    //----------------- Getter & Setter section -----------------
+    public int GetVerificationCounter()
+    {
+        return _verificationCounter;
+    }
+    public void SetVerificationCounter(int verificationCounterToSet)
+    {
+        _verificationCounter = verificationCounterToSet;
+    }
+    public int GetBonusGoalCompleted()
+    {
+        return _bonusGoalCompleted;
+    }
+    public void SetBonusGoalCompleted(int bonusGoalCompletedToSet)
+    {
+        _bonusGoalCompleted = bonusGoalCompletedToSet;
 
-        Console.Write("Write a short description? ");
-        string goalDescription = Console.ReadLine();
-        _description.Add(goalDescription);
+    }
+    public int GetCurrentlyCounter()
+    {
+        return _currentlyCounter;
+    }
+    public void SetCurrentlyCounter(int currenlyCounterToSet)
+    {
+        _currentlyCounter = currenlyCounterToSet;
 
-        Console.Write("How many points will be associated with this goal? ");
-        int times = int.Parse(Console.ReadLine());
+    }      //----------------- Getter & Setter section -----------------
+    //-------------------------- Methods ------------------------------
 
-        Console.Write("How many points will be associated with this goal? ");
-        int points = int.Parse(Console.ReadLine());
-        _gamepoints.Add(points);
+    // Override auto-implemented property with ordinary property
+    // to provide specialized accessor behavior.
+
+    //Method to set this value to a list
+    public override string GetGoalInformation()
+    {
+        if (_checked)
+        {
+            return $"[X] {_title} ({_description}) -- Currently completed {GetCurrentlyCounter()}/{_verificationCounter}";
+        }
+        else
+        {
+            return $"[ ] {_title} ({_description}) -- Currently completed {GetCurrentlyCounter()}/{_verificationCounter}";
+        }
     }
 
-    public void RecordEvent()
+    //Method to remplace the other information when the user complete 
+    //Add the points to the total
+    public override int GetGoalCompleted(int userPoints)
     {
-        int i = 0;
-        foreach (string item in _name)
+        //For each "check" +1 to the counter
+        _currentlyCounter = _currentlyCounter + 1;
+        int currentCounter = _currentlyCounter;
+        SetCurrentlyCounter(currentCounter);
+
+        string goalInformation = GetGoalInformation();
+        //$"[ ] {_title} ({_description}) -- Currently completed {GetCurrentlyCounter()}/{_verificationCounter}"
+
+        int index = _listOfGoals.IndexOf(goalInformation);
+        string newGoalInformation;
+
+        //When the counter is the same to the final check-counter
+        if (_currentlyCounter == _verificationCounter)
         {
-            i += 1;
-            Console.WriteLine($"{i}. {item}");
+            //Change checked to true
+            _checked = true;
+            newGoalInformation = GetGoalInformation();
+
+            //Add the bonus to the user's points
+            userPoints = userPoints + _bonusGoalCompleted;
+            //userPoints is the global variable userPoints
+            userPoints = userPoints + _pointsToComplete;
+        }
+        else if (_currentlyCounter > _verificationCounter)
+        {
+            _currentlyCounter = _currentlyCounter - 1;
+        }
+        else
+        {
+            newGoalInformation = goalInformation;
+            //$"[ ] {_title} ({_description}) -- Currently completed {GetCurrentlyCounter()}/{_verificationCounter}"
+            //userPoints is the global variable userPoints
+            userPoints = userPoints + _pointsToComplete;
         }
 
-        Console.Write("Which");
-
+        return userPoints;
 
     }
 
+    //Method to convert the object in a string (to save in an external file)
+    public override string GetStringRepresentation()
+    {
+        return "CheckListGoal:" + _title + "," + _description + "," + _pointsToComplete + "," + _verificationCounter + "," + _bonusGoalCompleted + "," + _currentlyCounter;
+    }
 }
--- prove/Develop05/SimpleGoals.cs
+++ prove/Develop05/SimpleGoals.cs
@@ -1,35 +1,73 @@
-﻿namespace Develop05;
+﻿using System;
 
-public class SimpleGoals : Goals
+//Public class to can use in "other window"
+public class SimpleGoal : Goal // SimpleGoal child class of Goal
 {
-    // private int _goalpoint;
-    // private int _goalname;
-    // private string _goaldescription;
+    //Protect class inside the propieties to not affect the working of the program (only the inherit class)
+    //Protect class inside the propieties to not affect the working of the program (only the inherit class)
+    //protected string _title;
+    //protected string _description;
+    //protected int _pointsToComplete;
+    //protected List<string> _listOfGoals = new List<string>();
+    private bool _checked = false;
+    private bool _completed = false;
+    //------------------ Constructors section (templates)--------------
 
-    public override void Play()
+    //The default SimpleGoal when we create (all the parameters from Goal)
+    public SimpleGoal(string goalName, string goalDescription, int pointsArchieved, bool checkedGoal = false) : base(goalName, goalDescription, pointsArchieved)
     {
-        Console.Write("What is the name of your goal? ");
-        string goalName = Console.ReadLine();
-        _name.Add(goalName);
+        //Default values
+        _title = goalName;
+        _description = goalDescription;
+        _pointsToComplete = pointsArchieved;
+        _checked = checkedGoal;
+    }
+    //------------------ Constructors section (templates)--------------
+    //-------------------------- Methods ------------------------------
 
-        Console.Write("Write a short description? ");
-        string goalDescription = Console.ReadLine();
-        _description.Add(goalDescription);
+    // Override auto-implemented property with ordinary property
+    // to provide specialized accessor behavior.
 
-        Console.Write("How many points will be associated with this goal? ");
-        int points = int.Parse(Console.ReadLine());
-        _gamepoints.Add(points);
+    //Method to set this value to a list
+    public override string GetGoalInformation()
+    {
+        {
+            if (_checked){
+                _completed = true;
+                return $"[x] {_title} ({_description})";}
+            else{
+                return $"[ ] {_title} ({_description})";}
+        }
     }
 
-    // public override void DisplayGoals()
-    // {
-    //     for (int i = 0; i < _description.Count; i++)
-    //     {
-    //         Console.WriteLine($"[ ] {_name[i]} ({_description[i]})");
-    //     }
-    // }
 
+    //Add the points to the total
+    public override int GetGoalCompleted(int userPoints)
+    {
+        //Change checked to true
+        bool completed = _completed;
 
-
-
+        _checked = true;
+        if (completed == false)
+        {
+            GetGoalInformation();
+            //Add the points one time
+            userPoints = userPoints + _pointsToComplete;
+            completed = true;
+            return userPoints;
+        }
+        else
+        {
+            //Not add more points in the same Goal
+            GetGoalInformation();
+            return userPoints;
+        }
+        
+    }
+    //Method to convert the object in a string (to save in an external file)
+    public override string GetStringRepresentation()
+    {
+        return "SimpleGoal:" + _title + "," + _description + "," + _pointsToComplete + "," + _checked;
+    }
 }
+
